54f2cd46e190c36f6920d762c83c8e01ed9fabb1
Adam Wrona <urbanoadam86@gmail.com>
2024-11-13 15:30:08 +0800

    Update server.js

--- server.js
+++ server.js
@@ -3,15 +3,15 @@ const mysql = require('mysql');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Ustawienia bazy danych MySQL
+
 const db = mysql.createConnection({
-    host: 'portfoliodb.c18s48a0gb7h.us-east-1.rds.amazonaws.com', // Zastąp tym endpointem bazy danych RDS
-    user: 'admin', // Nazwa użytkownika bazy danych
-    password: 'etnaD232!', // Hasło do bazy danych
-    database: 'portfolioDB' // Nazwa bazy danych
+    host: 'portfoliodb.c18s48a0gb7h.us-east-1.rds.amazonaws.com', 
+    user: 'admin', 
+    password: 'Admin1234!', 
+    database: 'portfolioDB' 
 });
 
-// Połączenie z bazą danych
+
 db.connect((err) => {
     if (err) throw err;
     console.log('Połączono z bazą danych MySQL');
@@ -19,16 +19,16 @@ db.connect((err) => {
 
 app.use(express.json());
 
-// Endpoint testowy
+
 app.get('/', (req, res) => {
     res.send('Server działa! Witamy na backendzie portfolia.');
 });
 
-// Endpoint POST /contact do zapisu wiadomości kontaktowych
+
 app.post('/contact', (req, res) => {
     const { name, email, message } = req.body;
 
-    // Sprawdzenie, czy wszystkie wymagane dane są w zapytaniu
+   
     if (!name || !email || !message) {
         return res.status(400).send('Brak wymaganych danych!');
     }
