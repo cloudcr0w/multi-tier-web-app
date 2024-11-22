6fa814c36cf059c0efffac4030c0ee4772f2b6e4
cloudcr0w <adamwronowy@gmail.com>
2024-11-22 17:54:59 +0800

    Updated frontend and server configuration for EC2

--- frontend/script.js
+++ frontend/script.js
@@ -1,29 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const form = document.querySelector('#contact-form');
-    form.addEventListener('submit', async e => {
-        e.preventDefault(); // Zapobiega domyślnej wysyłce formularza
+	const form = document.querySelector('#contact-form')
+	form.addEventListener('submit', async e => {
+		e.preventDefault()
 
-        const name = document.querySelector('#name').value;
-        const email = document.querySelector('#email').value;
-        const message = document.querySelector('#message').value;
+		const name = document.querySelector('#name').value.trim()
+		const email = document.querySelector('#email').value.trim()
+		const message = document.querySelector('#message').value.trim()
 
-        try {
-            const response = await fetch('https://api.crow-project.click/contact', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ name, email, message }),
-            });
+		console.log('Form data:', { name, email, message })
 
-            if (response.ok) {
-                alert('Message sent successfully!');
-            } else {
-                alert('Error: ' + response.statusText);
-            }
-        } catch (err) {
-            console.error('Error:', err);
-            alert('Error sending message. Please check your network connection.');
-        }
-    });
-});
+		try {
+			const response = await fetch('http://3.93.195.12:3000/contact', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({ name, email, message }),
+			
+			})
+
+			if (response.ok) {
+				alert('Message sent successfully!')
+			} else {
+				alert('Error: ' + response.statusText)
+			}
+		} catch (err) {
+			console.error('Error:', err)
+			alert('Error sending message. Please check your network connection.')
+		}
+	})
+})
